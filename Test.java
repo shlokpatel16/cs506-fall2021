d7c109e91c2ac6fb78acbafdd6d9f0f7e390dd38
schen921 <48606083+schen921@users.noreply.github.com>
2021-10-09 20:30:25 -0500

    added solution method to return correct solution

--- Test.java
+++ Test.java
@@ -4,4 +4,13 @@ class Test {
 		int testOnepass = trading_Onepass.maxProfit(nums);
 		return testNaive == testOnepass;
 	}
+	
+	private static int solution(int[] prices) {
+		int maxCur = 0, maxSoFar = 0;
+		for(int i = 1; i < prices.length; i++) {
+		    maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+		    maxSoFar = Math.max(maxCur, maxSoFar);
+		}
+		return maxSoFar;
+	}
 }
