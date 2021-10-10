5f0af797a1e24854940175db8e91ef89473047c6
schen921 <48606083+schen921@users.noreply.github.com>
2021-10-09 20:28:06 -0500

    student main function

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+	public static void main(String[] args) {
+		int[] prices = {1,0,9,5,18,3,15,34,5,9,7,45,182,0,8,7,3,6,9,7,15,45,76,95,89};
+		// TODO: Test the two implementations - naive and onepass below
+		
+	}
+}
