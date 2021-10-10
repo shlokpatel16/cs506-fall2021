0a4ef302c26e6e8f3568e85bf051a54a09158fd7
schen921 <48606083+schen921@users.noreply.github.com>
2021-10-09 20:30:51 -0500

    Added test cases for naive and onepass methods

--- Test.java
+++ Test.java
@@ -1,8 +1,9 @@
 class Test {
 	public static boolean test(int[] nums) {
+		int sol = solution(nums);
 		int testNaive = trading_Naive.maxProfit(nums);
 		int testOnepass = trading_Onepass.maxProfit(nums);
-		return testNaive == testOnepass;
+		return testNaive == sol && testOnepass == sol;
 	}
 	
 	private static int solution(int[] prices) {
